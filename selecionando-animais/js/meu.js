0b17cbd6da9cd016562085873e7597fc47ddf3d9
samuelbatista04 <cp.websam@gmail.com>
2022-10-16 15:21:18 -0300

    modified:   selecionando-animais/css/meu.css 	modified:   selecionando-animais/index.html 	modified:   selecionando-animais/js/meu.js

--- selecionando-animais/js/meu.js
+++ selecionando-animais/js/meu.js
@@ -1,23 +1,32 @@
+// Selecionando botões, animais e versos no html
 const listaBtn = document.querySelectorAll('#menu-botoes li')
 const listaAnimal = document.querySelectorAll('#lista-animais li')
 const listaVerso = document.querySelectorAll('#lista-versos li')
 
-function ativarAnimal(index) {
-    listaAnimal.forEach((imgAnimal) => {
-        imgAnimal.classList.remove('ativo')
+if (listaBtn.length && listaAnimal.length && listaVerso) {
+    // itens que iniciam ja ativos
+    listaAnimal[0].classList.add('ativo')
+    listaVerso[0].classList.add('ativo')
+
+    // Função que seleciona os animais e os versos
+    function ativarAnimal(index) {
+        listaAnimal.forEach((imgAnimal) => {
+            imgAnimal.classList.remove('ativo')
+        })
+
+        listaAnimal[index].classList.add('ativo')
+
+        listaVerso.forEach((verso) => {
+            verso.classList.remove('ativo')
+        })
+
+        listaVerso[index].classList.add('ativo')
+    }
+
+    // Loop com evento de click que aciona os botoes
+    listaBtn.forEach((botao, index) => {
+        botao.addEventListener('click', () => {
+            ativarAnimal(index)
+        })
     })
-
-    listaAnimal[index].classList.add('ativo')
-
-    listaVerso.forEach((verso) => {
-        verso.classList.remove('ativo')
-    })
-
-    listaVerso[index].classList.add('ativo')
 }
-
-listaBtn.forEach((botao, index) => {
-    botao.addEventListener('click', () => {
-        ativarAnimal(index)
-    })
-})
